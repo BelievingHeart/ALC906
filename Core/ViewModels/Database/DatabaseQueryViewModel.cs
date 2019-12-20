f165c9967c3c1c80e8d010ef809a3b7bd47f6a23
afterburner <rockey1024@icloud.com>
2019-12-20 20:19:28 +0800

    Generated pie chart successfully

--- Core/ViewModels/Database/DatabaseQueryViewModel.cs
+++ Core/ViewModels/Database/DatabaseQueryViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
 using System.Windows.Input;
 using Core.Constants;
@@ -20,15 +21,32 @@ namespace Core.ViewModels.Database
         private ProductType _productType;
         private bool _isHourEnable;
         private DatabaseContentPageType _currentDatabaseContentPage;
+        private Dictionary<string, int> _pieChartData;
 
         #endregion
 
         #region prop
 
+        public bool IsBusyGeneratingPieChart { get; set; }
+
+        public Type CollectionType { get; set; }
+
+        public Dictionary<string, int> PieChartData
+        {
+            get => _pieChartData;
+            set
+            {
+                _pieChartData = value;
+                if (value != null) ShouldDisplayPieChart = true;
+            }
+        }
+
+        public bool ShouldDisplayPieChart { get; set; }
+
         public Dictionary<string, double> DictionaryUpper { get; set; }
         public Dictionary<string, double> DictionaryLower { get; set; }
 
-        public DatabaseBufferViewModel DatabaseBuffer { get; set; }     
+        public DatabaseBufferViewModel DatabaseBuffer { get; set; }
 
         public int Year { get; set; } = int.Parse(DateTime.Now.ToString("yyyy"));
 
@@ -48,11 +66,12 @@ namespace Core.ViewModels.Database
                 OnPropertyChanged(nameof(ProductType));
             }
         }
-        
+
         public List<FaiLimitViewModel> FaiLimits { get; set; }
 
 
         public ICommand QueryByHourCommand { get; }
+
         /// <summary>
         /// Do temporary tests 
         /// </summary>
@@ -62,10 +81,7 @@ namespace Core.ViewModels.Database
 
         public bool IsHourEnable
         {
-            get
-            {
-                return IsDayEnable && _isHourEnable;
-            }
+            get { return IsDayEnable && _isHourEnable; }
             set { _isHourEnable = value; }
         }
 
@@ -89,60 +105,116 @@ namespace Core.ViewModels.Database
 
         public ICommand SwitchSettingViewCommand { get; set; }
 
-
-
+        public ICommand GenPieChartCommand { get; }
 
         #endregion
-
 
 
         #region Constructor
 
         public DatabaseQueryViewModel()
         {
-            QueryByHourCommand = new RelayCommand(()=>RunOnlySingleFireIsAllowedEachTimeCommand(()=>IsBusyQuerying, QueryByHourAsync));
-            SwitchSettingViewCommand = new SimpleCommand(o=>CurrentDatabaseContentPage = DatabaseContentPageType.SettingPage, o=>CurrentDatabaseContentPage!=DatabaseContentPageType.SettingPage);
-            SwitchTablesViewCommand = new SimpleCommand(o=>CurrentDatabaseContentPage = DatabaseContentPageType.TablePage, o=>CurrentDatabaseContentPage!=DatabaseContentPageType.TablePage);
-            
+            QueryByHourCommand = new RelayCommand(() =>
+                RunOnlySingleFireIsAllowedEachTimeCommand(() => IsBusyQuerying, QueryByHourAsync));
+            SwitchSettingViewCommand =
+                new SimpleCommand(o => CurrentDatabaseContentPage = DatabaseContentPageType.SettingPage,
+                    o => CurrentDatabaseContentPage != DatabaseContentPageType.SettingPage);
+            SwitchTablesViewCommand =
+                new SimpleCommand(o => CurrentDatabaseContentPage = DatabaseContentPageType.TablePage,
+                    o => CurrentDatabaseContentPage != DatabaseContentPageType.TablePage);
+            GenPieChartCommand = new SimpleCommand(o=>RunOnlySingleFireIsAllowedEachTimeCommand(()=>IsBusyGeneratingPieChart, GenPieChartDataAsync),
+                o => DatabaseBuffer.FaiCollectionBuffers != null && DatabaseBuffer.FaiCollectionBuffers.Count > 0);
+
             DatabaseBuffer = new DatabaseBufferViewModel();
-            
+
             DoSimulationCommand = new RelayCommand(DoSimulation);
-            
+
             LoadFaiLimits(_productType);
             GenerateLimitDictionaries();
+        }
+
+        /// <summary>
+        /// Loop through all the fai properties in FaiCollectionBuffer
+        /// and count for the Ng occurence for each fai property
+        /// remove those with 0 count
+        /// </summary>
+        private async Task GenPieChartDataAsync()
+        {
+            var output = new Dictionary<string, int>();
+
+            await Task.Run(() =>
+            {
+                // Init PieChartData with all 0s
+                foreach (var key in DictionaryLower.Keys)
+                {
+                    output[key] = 0;
+                }
+
+                // Accumulate counts for NG fai items
+                foreach (var collection in DatabaseBuffer.FaiCollectionBuffers)
+                {
+                    foreach (var property in CollectionType.GetProperties())
+                    {
+                        if (!IsFaiProp(property)) continue;
+                        var propName = property.Name;
+                        var propValue = (double) property.GetValue(collection);
+                        if (IsFaiPropNg(propValue, propName, DictionaryLower, DictionaryUpper))
+                            output[propName]++;
+                    }
+                }
+
+                // Prune fai items with 0 Ng count
+                foreach (var item in output.Where(kvp => kvp.Value == 0).ToList())
+                {
+                    output.Remove(item.Key);
+                }
+            });
+            
+            PieChartData = output;
+        }
+
+
+        private bool IsFaiPropNg(double propValue, string propName, Dictionary<string, double> dictionaryLower,
+            Dictionary<string, double> dictionaryUpper)
+        {
+            return propValue < dictionaryLower[propName] || propValue > dictionaryUpper[propName];
         }
 
         #endregion
 
         private void DoSimulation()
         {
-           DatabaseBuffer.FaiCollectionBuffers = new List<IFaiCollection>()
+            DatabaseBuffer.FaiCollectionBuffers = new List<IFaiCollection>()
             {
-                new FaiCollectionTest(){Cavity = 1, InspectionTime = "SomeTime", Result = "SomeResult", Test = "Hello"}
+                new FaiCollectionTest() {Cavity = 1, InspectionTime = "SomeTime", Result = "SomeResult", Test = "Hello"}
             };
         }
 
-        private  async Task QueryByHourAsync()
+        private async Task QueryByHourAsync()
         {
-
             DatabaseBuffer.FaiCollectionBuffers = await Task.Run(() => FaiCollectionHelper.SelectByHour(ProductType,
                 NameConstants.SqlConnectionString, Year, Month,
                 Day, Hour));
         }
-        
+
         private void LoadFaiLimits(ProductType productType)
         {
-            var collectionType = productType == ProductType.Mtm ? typeof(FaiCollectionMtm) : typeof(FaiCollectionAlps);
-            var propNames = GetFaiPropNames(collectionType);
+            CollectionType = productType == ProductType.Mtm ? typeof(FaiCollectionMtm) : typeof(FaiCollectionAlps);
+            var propNames = GetFaiPropNames(CollectionType);
             FaiLimits = AutoSerializableHelper
                 .LoadAutoSerializables<FaiLimitViewModel>(propNames, DirectoryConstants.DatabaseLimitsDirs[productType])
                 .ToList();
         }
 
+
         private IEnumerable<string> GetFaiPropNames(Type collectionType)
         {
-            return collectionType.GetProperties().Where(prop => prop.Name.Contains("FAI")).Select(info => info.Name);
+            return collectionType.GetProperties().Where(IsFaiProp).Select(info => info.Name);
         }
-        
+
+        private bool IsFaiProp(PropertyInfo propertyInfo)
+        {
+            return propertyInfo.Name.Contains("FAI");
+        }
     }
 }
---
+++ DatabaseQuery/Views/PieChartView.xaml.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using LiveCharts;
+using LiveCharts.Wpf;
+
+namespace DatabaseQuery.Views
+{
+    public partial class PieChartView : UserControl
+    {
+        public PieChartView()
+        {
+            InitializeComponent();
+            
+            PointLabel = chartPoint =>
+                $"{chartPoint.Y} ({chartPoint.Participation:P})";
+        }
+
+        #region PointLabel
+
+        public static readonly DependencyProperty PointLabelProperty = DependencyProperty.Register(
+            "PointLabel", typeof(Func<ChartPoint,string>), typeof(PieChartView), new PropertyMetadata(default(Func<ChartPoint,string>)));
+
+        public Func<ChartPoint,string> PointLabel
+        {
+            get { return (Func<ChartPoint,string>) GetValue(PointLabelProperty); }
+            set { SetValue(PointLabelProperty, value); }
+        }
+
+
+        private void Chart_OnDataClick(object sender, ChartPoint chartpoint)
+        {
+            var chart = (PieChart) chartpoint.ChartView;
+            
+            //clear selected slice.
+            foreach (var seriesView in chart.Series)
+            {
+                var series = (PieSeries) seriesView;
+                series.PushOut = 0;
+            }
+
+            var selectedSeries = (PieSeries) chartpoint.SeriesView;
+            selectedSeries.PushOut = 8;
+        }
+
+        #endregion
+
+        #region PieChartData
+
+        public static readonly DependencyProperty PieChartDataProperty = DependencyProperty.Register(
+            "PieChartData", typeof(Dictionary<string,int>), typeof(PieChartView), new PropertyMetadata(default(Dictionary<string,int>), OnPieChartDataChanged));
+
+        private static void OnPieChartDataChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            var view = (PieChartView) d;
+            var newData = (Dictionary<string, int>) e.NewValue;
+            if(newData == null) return;
+
+            view.GenPieChart(newData);
+        }
+
+        private void GenPieChart(Dictionary<string,int> pieChartData)
+        {
+            PART_PieChartControl.Series.Clear();
+            
+            var pointLabelBinding = new Binding(nameof(PointLabel)){Source = this};
+            foreach (var key in pieChartData.Keys)
+            {
+                var series = new PieSeries() {Title = key, Values = new ChartValues<int>(new[] {pieChartData[key]})};
+                series.SetBinding(PointLabelProperty, pointLabelBinding);
+                PART_PieChartControl.Series.Add(series);
+            }
+        }
+
+        public Dictionary<string,int> PieChartData
+        {
+            get { return (Dictionary<string,int>) GetValue(PieChartDataProperty); }
+            set { SetValue(PieChartDataProperty, value); }
+        }
+
+        #endregion
+    }
+}
